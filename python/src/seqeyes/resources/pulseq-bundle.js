7f4a083a5a7a6c2ac7bf3cf5ed28371b78ec2cdc
Yiyun Dong <dyyakiko@gmail.com>
2026-07-23 22:21:13 -0400

    chore: bump version to 0.2.11

--- python/src/seqeyes/resources/pulseq-bundle.js
+++ python/src/seqeyes/resources/pulseq-bundle.js
@@ -53,7 +53,7 @@ var Pulseq = (() => {
   });
 
   // package.json
-  var version = "0.2.9";
+  var version = "0.2.11";
 
   // src/pulseq/decompressor.ts
   function decompressShape(compressed, numSamples) {
